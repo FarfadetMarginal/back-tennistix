321399a4c0f701f341d059ce7219bcb6f5e692c0
FarfadetMarginal <bapt935@gmail.com>
2026-09-15 16:23:39 +0200

    cors front dev added

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 const corsOption = {
-    origin: ['http://localhost:3004']
+    origin: ['http://localhost:3004', 'http://localhost:5173']
 }
 app.use(cors(corsOption))
 
