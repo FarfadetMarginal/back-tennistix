cd2fe2300cfd795a0ed8619a0736bba6d7515df9
FarfadetMarginal <bapt935@gmail.com>
2026-09-16 12:25:11 +0200

    ssl port change didnt work ; back to 587

--- tools/mailSender.js
+++ tools/mailSender.js
@@ -3,8 +3,8 @@ const nodemailer = require("nodemailer");
 async function mailSender(email, username, token) {
     const transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
+        port: 587,
+        secure: false,
         auth: {
             user: process.env.GOOGLE_MAIL,
             pass: process.env.GOOGLE_PASS,
